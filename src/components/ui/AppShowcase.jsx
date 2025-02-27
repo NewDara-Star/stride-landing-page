7cc459a201bfe27db8eb77f55e8c8916c3a4f1e2
NewDara-Star <139634597+NewDara-Star@users.noreply.github.com>
2025-02-27 20:59:08 +0000

    Refactor AppShowcase and HowItWorks components to use boolean for direction prop; add flex-row-reverse class for layout adjustments

--- src/components/ui/AppShowcase.jsx
+++ src/components/ui/AppShowcase.jsx
@@ -5,11 +5,11 @@ AppShowcase.propTypes = {
     imageUrl: PropTypes.string.isRequired,
     headerText: PropTypes.string.isRequired,
     bodyText: PropTypes.string.isRequired,
-    direction: PropTypes.string.isRequired
+    direction: PropTypes.bool
 };
 function AppShowcase  ({ imageUrl, headerText, bodyText, direction }) {
     return (
-        <section className={`flex flex-col items-center p-8 justify-around gap-16 mx-8 md:${direction}`} >
+        <section className={`flex flex-col items-center p-8 justify-around gap-16 mx-8 md:${direction ? 'flex-row-reverse' : 'flex-row'}`} >
             <img src={imageUrl} alt="App showcase" className=' w-1/2 md:w-1/4'/>
             
                 <div className="flex flex-col items-start justify-start min-w-28w-full md:w-1/2 ">
